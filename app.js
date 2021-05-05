39ddd93b36a995a0eca1acc809c372aba9c7cbf3
Stephanie Taylor <sjean.taylor5@gmail.com>
2021-05-05 07:32:49 -0400

    Set up initial project structure. Created APIs

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const usersRouter = require('./routes/users.js');
 
-const { PORT = 3000 } = process.env;
 const app = express();
 
+const { PORT = 3000 } = process.env;
+
+app.use('/', usersRouter);
+
 app.listen(PORT, () => {
-    console.log(`app listening at port ${PORT}`);
+  console.log(`app listening at port ${PORT}`);
 });
